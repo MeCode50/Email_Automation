398575fc92a562f56b3c3ed269fbf628a26db986
Muna <victorycollins31@gmail.com>
2022-09-26 01:35:57 +0100

    demo index.html file

--- mail-demo.py
+++ mail-demo.py
@@ -10,18 +10,9 @@ import smtplib
 email_sender = 'emailtesting437@gmail.com'
 #get python app password saved on our enviroment variable. 
 email_password = os.environ.get('PYTHON_PASS')
-# create a variable for the email receieving the message
+## create a variable for the email receieving the message
 email_receiever = [ 'victorycollins31@gmail.com'] 
 # crewte variale for the title of the email
-subject = 'testing the email'
-
-# create the body/content of the mail
-body = """"
-Hello Max, you receieved this mail becuase my email automation with python just worked. 
-I am so xcited. 
-"""
-
-
 
 # instantiate EmailMessage.
 # em stands for the object we'll use to write the email
@@ -29,9 +20,26 @@ em = EmailMessage()
 # define some elements of the email
 em['from'] = email_sender
 em['to'] = email_receiever
-em['subject'] = subject
+em['subject'] = 'check this out'
+
+
 # use set_content method to define the body of the mail
-em.set_content(body)
+em.set_content('This is a plain text email')
+
+em.add_alternative("""\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+</head>
+<body>
+    <h1 style="color:blue;">This is a testing email</h1>
+</body>
+</html>
+""", subtype='html')
 
 # add a layer of security using ssl
 context = ssl.create_default_context()
